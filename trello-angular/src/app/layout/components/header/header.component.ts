f5b272dd375767089cb7a93dca2f42310dc15cbb
Viet Nguyen Huy <vietnh.161@gmail.com>
2021-12-16 22:31:04 +0700

    add btn and popover create board

--- trello-angular/src/app/layout/components/header/header.component.ts
+++ trello-angular/src/app/layout/components/header/header.component.ts
@@ -12,6 +12,26 @@ export class HeaderComponent implements OnInit {
   @Input() sidebarItems: any[] = [];
   sidebarSelected: any;
   currentScreenSize: DeviceScreenType = null;
+
+  createActions = [
+    {
+      ID: 1,
+      Title: 'Create board',
+      Description:'A board is made up of cards ordered on lists. Use it to manage projects, track information, or organize anything.'
+    },
+    {
+      ID: 2,
+      Title: 'Start with a template',
+      Description:'Get started faster with a board template.'
+    },
+    {
+      ID: 3,
+      Title: 'Create Workspace',
+      Description:'A Workspace is a group of boards and people. Use it to organize your company, side hustle, family, or friends.'
+    }
+  ]
+  currentAction = null;
+
   constructor(private layoutService: LayoutService) {
     
   }
@@ -20,5 +40,10 @@ export class HeaderComponent implements OnInit {
     this.layoutService.getCurrentScreenSize().subscribe((type) => {
       this.currentScreenSize = type;
     });
+  }
+
+
+  handleClickAction(createAction: any){
+    this.currentAction = createAction.ID;
   }
 }
